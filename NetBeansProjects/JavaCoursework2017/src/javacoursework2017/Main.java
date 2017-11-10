117a483fa83a0ec64c975b050aadb6b971569c53
Samuel Reeve <sreeve96@googlemail.com>
2017-11-10 19:11:28 +0000

    started Pipetypechecker pseudo

--- NetBeansProjects/JavaCoursework2017/src/javacoursework2017/Main.java
+++ NetBeansProjects/JavaCoursework2017/src/javacoursework2017/Main.java
@@ -27,37 +27,59 @@ public class Main {
     basket.add(newPipe);
     }
     
-    public static void testAllPipeTypes(){
-      
-//test creation of each pipe type
-      Pipe pipeOrder0 = new Pipe(3, 0, 10, false, false, true, 10.00, 5.00);
-      pipeOrder0.calcBaseCost();
-      pipeOrder0.calcAdditonalCost();
-      System.out.println("\n");
-      
-      PipeTypeOne pipeOrder1 = new PipeTypeOne(3, 10, true, 10.00, 5.00);
-      pipeOrder1.calcBaseCost();
-      pipeOrder1.calcAdditonalCost();
-      System.out.println("\n");
-      
-      PipeTypeTwo pipeOrder2 = new PipeTypeTwo(3, 10, true, 10.00, 5.00);
-      pipeOrder2.calcBaseCost();
-      pipeOrder2.calcAdditonalCost();
-      System.out.println("\n");
-      
-      PipeTypeThree pipeOrder3 = new PipeTypeThree(3, 10, true, 10.00, 5.00);
-      pipeOrder3.calcBaseCost();
-      pipeOrder3.calcAdditonalCost();
-      System.out.println("\n");
-      
-      PipeTypeFour pipeOrder4 = new PipeTypeFour(3, 10, true, 10.00, 5.00);
-      pipeOrder4.calcBaseCost();
-      pipeOrder4.calcAdditonalCost();
-      System.out.println("\n");
-      
-      PipeTypeFive pipeOrder5 = new PipeTypeFive(3, 10, true, 10.00, 5.00);
-      pipeOrder5.calcBaseCost();
-      pipeOrder5.calcAdditonalCost();
-      System.out.println("\n");
+    public void newPipe() {
+        checkPipeType();
+    }
+    
+    public void checkPipeType() {
+        //Type 1 check
+        if (grade <= 3 && colour == 0 && InnerInsulation = false && OuterReinforcement = false){
+            //Its pipe one!
+        }
+        else if (2 <= grade <= 4 && colour == 1 && InnerInsulation = false && OuterReinforcement = false) {
+            //its pipe two!
+        }
+        else if (2 <= grade <= 5 && colour == 2 && InnerInsulation = false && OuterReinforcement = false) {
+            //its pipe three!
+        }
+        else if (2 <= grade <= 5 && colour == 2 && InnerInsulation = true && OuterReinforcement = false) {
+            //its pipe four!
+        }
+        else if (3 <= grade <= 5 && colour == 2 && InnerInsulation = true && OuterReinforcement = true) {
+            //its pipe four!
+        }
+    }
+    
+    public static void testAllPipeTypes(){  
+        //test creation of each pipe type
+        Pipe pipeOrder0 = new Pipe(3, 0, 10, false, false, true, 10.00, 5.00);
+        pipeOrder0.calcBaseCost();
+        pipeOrder0.calcAdditonalCost();
+        System.out.println("\n");
+
+        PipeTypeOne pipeOrder1 = new PipeTypeOne(3, 10, true, 10.00, 5.00);
+        pipeOrder1.calcBaseCost();
+        pipeOrder1.calcAdditonalCost();
+        System.out.println("\n");
+
+        PipeTypeTwo pipeOrder2 = new PipeTypeTwo(3, 10, true, 10.00, 5.00);
+        pipeOrder2.calcBaseCost();
+        pipeOrder2.calcAdditonalCost();
+        System.out.println("\n");
+
+        PipeTypeThree pipeOrder3 = new PipeTypeThree(3, 10, true, 10.00, 5.00);
+        pipeOrder3.calcBaseCost();
+        pipeOrder3.calcAdditonalCost();
+        System.out.println("\n");
+
+        PipeTypeFour pipeOrder4 = new PipeTypeFour(3, 10, true, 10.00, 5.00);
+        pipeOrder4.calcBaseCost();
+        pipeOrder4.calcAdditonalCost();
+        System.out.println("\n");
+
+        PipeTypeFive pipeOrder5 = new PipeTypeFive(3, 10, true, 10.00, 5.00);
+        pipeOrder5.calcBaseCost();
+        pipeOrder5.calcAdditonalCost();
+        System.out.println("\n");
     }
 }
